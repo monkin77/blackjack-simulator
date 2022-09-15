959010e4a5619a88fdbd8194485bb4d099d09ff7
Monkin <monkinsmurf@gmail.com>
2022-09-15 14:10:59 +0100

    Add: Basic Frontend server to test API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { getTest } from "./services/test";
 
 function App() {
+    const [data, setData] = useState("Hello World!");
+
+    useEffect(() => {
+        getTest()
+            .then((res) => {
+                console.log("Got res: ", res);
+                setData(res.message);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <div className="App">
-            <h1>Hello World!</h1>
+            <h1>{data}</h1>
         </div>
     );
 }
---
+++ frontend/src/services/test.js
@@ -0,0 +1,15 @@
+export const getTest = async() => {
+    try {
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/test`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+
+        return await res.json();
+    } catch (err) {
+        console.log("Error in getTest service: ", err);
+    }
+};
