eeea27ebe189ea20dccffc4b21fbdbdca66d0fe0
Monkin <monkinsmurf@gmail.com>
2022-09-15 23:53:47 +0100

    Add: Add backend validators

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import User from "../models/User.js";
+import { registerValidation } from "../validators/auth.js";
 
 const router = Router();
 
@@ -7,6 +8,14 @@ export default (mainRouter) => {
     mainRouter.use("/auth", router);
 
     router.post("/register", async(req, res) => {
+        // Validate the data before creating a user
+        const { error } = registerValidation(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        // Checking if the user is already in the DB
+        const emailExists = await User.findOne({ email: req.body.email });
+        if (emailExists) return res.status(400).send("Email already exists");
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
---
+++ backend/validators/auth.js
@@ -0,0 +1,21 @@
+import Joi from "@hapi/joi";
+
+// Register Validation
+export const registerValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).max(255).required(),
+        email: Joi.string().min(4).max(255).email().required(),
+        password: Joi.string().min(6).max(1024).required(),
+    });
+
+    return schema.validate(data);
+};
+
+export const loginValidation = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().min(4).max(255).email().required(),
+        password: Joi.string().min(6).max(1024).required(),
+    });
+
+    return schema.validate(data);
+};
